acdd48655795f4e6e846b4d4b65fbfa46de70280
Archie Martin <archie.cgm@gmail.com>
2023-10-24 11:10:57 +0100

    updated nav to receive route changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,25 +7,25 @@ import Image from 'next/image'
 
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
-      const response = getProviders();
+    const setUpProviders = async () => {
+      const response = await getProviders();
       setProviders(response)
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src='/assets/images/logo.svg'
+          src={session?.user.image}
           alt='logo'
           width={30}
           height={30}
@@ -36,7 +36,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ?(
+        {session?.user ?(
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create post
@@ -78,10 +78,10 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image 
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
