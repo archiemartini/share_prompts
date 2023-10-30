1fe84f3bd9ff65746c262425cda92b4762bff8f2
Archie Martin <archie.cgm@gmail.com>
2023-10-30 10:55:36 +0000

    implemented search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -19,23 +19,52 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('')
   const [posts, setPosts] = useState([])
-
-  const handleSearchChange = (e) => {
-
-  }
-
+  
+  const [searchText, setSearchText] = useState('')
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState(null)
+  
   const fetchPosts = async () => {
     const response = await fetch('api/prompt');
     const data = await response.json();
-
+    
     setPosts(data)
   }
-
+  
   useEffect(() => {
     fetchPosts();
   }, [])
+  
+  const filterPrompts = (searchText) => {
+    const regex = new RegExp(searchText, 'i');
+    
+    return posts.filter(
+      (item) =>
+      regex.test(item.creator.username) ||
+      regex.test(item.tag) ||
+      regex.test(item.prompt)
+      )
+  }
+    
+  const handleSearchChange = (e) => {
+      clearTimeout(searchTimeout);
+      setSearchText(e.target.value);
+
+      setSearchTimeout(
+        setTimeout(() => {
+          const searchResult = filterPrompts(e.target.value);
+          setSearchedResults(searchResult)
+        }, 500)
+      )
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
     <section className='feed'>
@@ -49,10 +78,16 @@ const Feed = () => {
           className='search_input peer'
         />
       </form>
-
-      <PromptCardList 
-        data={posts}
-      />
+      {searchedResults ? 
+        <PromptCardList 
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        /> : (
+        <PromptCardList 
+          data={posts}
+          handleTagClick={handleTagClick}
+        />
+      )}
     </section>
   )
 }
